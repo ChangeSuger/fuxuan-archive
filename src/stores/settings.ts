746eb94b83da0cd0dcdd4add704dd80942f45333
Camille <camille_0315@buaa.edu.cn>
2023-11-17 13:40:32 +0800

    feat(theme): 颜色主题默认跟随浏览器设置 (#6)

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,8 +1,9 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { ref, computed, watch, onMounted } from 'vue';
+import { usePreferredDark } from '@vueuse/core';
 import type { Language } from '@/types/Settings';
 
-type Theme = 'light' | 'dark';
+type Theme = 'light' | 'dark' | 'auto';
 
 const textjoinInit = {
   54: -262052143,
@@ -13,7 +14,14 @@ export const useSettingsStore = defineStore(
   () => {
     const lang = ref('CHS' as Language);
     const textjoin = ref(textjoinInit as { [id: number | string]: number; });
-    const theme = ref<Theme>('dark');
+    const theme = ref<Theme>('auto');
+    const isDark = usePreferredDark();
+
+    const getTheme = computed<Theme>(() => {
+      return theme.value === 'auto'
+        ? (isDark.value ? 'dark' : 'light')
+        : theme.value;
+    });
 
     function setLang (_lang: Language) {
       lang.value = _lang;
@@ -21,23 +29,41 @@ export const useSettingsStore = defineStore(
     function setTextjoin (id: number | string, hash: number) {
       textjoin.value[id] = hash;
     }
-    function setTheme (_theme: Theme) {
-      if (_theme === 'light') {
-        document.body.removeAttribute('arco-theme');
+    function toggleTheme () {
+      if (getTheme.value === 'dark') {
+        theme.value = isDark.value ? 'light' : 'auto';
       } else {
+        theme.value = isDark.value ? 'auto' : 'dark';
+      }
+    }
+
+    watch(
+      () => getTheme.value,
+      (theme) => {
+        if (theme === 'light') {
+          document.body.removeAttribute('arco-theme');
+        } else {
+          document.body.setAttribute('arco-theme', 'dark');
+        }
+      }
+    );
+
+    onMounted(() => {
+      if (getTheme.value === 'dark') {
         document.body.setAttribute('arco-theme', 'dark');
       }
-      theme.value = _theme;
-    }
+    });
 
     return {
       lang,
       textjoin,
       theme,
 
+      getTheme,
+
       setLang,
       setTextjoin,
-      setTheme,
+      toggleTheme,
     }
   },
   {
